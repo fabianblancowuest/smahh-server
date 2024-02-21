e297817067dce36e37a7b8aa3bde8251d582d256
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2024-02-21 19:59:04 -0300

    configured database connection

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,32 +1,28 @@
-const { Sequelize } = require('sequelize');
+require("dotenv").config();
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { Sequelize } = require("sequelize");
 const TicketModel = require("./models/Ticket");
-const UserModel = require("./models/User")
+const UserModel = require("./models/User");
 
 const sequelize = new Sequelize(
-   "postgres://postgres:1234@localhost:5432/securityDB",
-   { logging: false, native: false }
+	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+	{ logging: false, native: false },
 );
-
-// EJERCICIO 05
-// Debajo de este comentario puedes ejecutar la función de los modelos.
 
 TicketModel(sequelize);
 UserModel(sequelize);
 
-
-// ¡Relaciona tus modelos aquí abajo!
 const { Ticket, User } = sequelize.models;
 
 User.hasMany(Ticket, {
-   foreignKey: {
-      name: "UserId",
-   }
+	foreignKey: {
+		name: "UserId",
+	},
 });
 Ticket.belongsTo(User);
 
-
 module.exports = {
-   User,
-   Ticket,
-   conn: sequelize,
+	User,
+	Ticket,
+	conn: sequelize,
 };
