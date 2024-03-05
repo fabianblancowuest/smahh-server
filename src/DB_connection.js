579dfb5e45df5de507ca12d51f18a4e63ee6ae4c
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2024-03-04 21:32:08 -0300

    added database environment variable

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } =
+	process.env;
 const { Sequelize } = require("sequelize");
 const TicketModel = require("./models/Ticket");
 const UserModel = require("./models/User");
 
-const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-	{ logging: false, native: false },
-);
+// const sequelize = new Sequelize(
+// 	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+// 	{ logging: false, native: false },
+// );
+
+const sequelize = new Sequelize(DB_DEPLOY, { logging: false, native: false });
 
 TicketModel(sequelize);
 UserModel(sequelize);
